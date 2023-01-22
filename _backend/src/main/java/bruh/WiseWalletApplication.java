2227d95e62baa60f77f2df4d3d2de0a242e2dd96
aloisorozco <alois.orozco@gmail.com>
2023-01-22 00:42:07 -0500

    added corsFilter

--- _backend/src/main/java/bruh/WiseWalletApplication.java
+++ _backend/src/main/java/bruh/WiseWalletApplication.java
@@ -2,12 +2,24 @@ package bruh;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class WiseWalletApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WiseWalletApplication.class, args);
+	}
+
+	@Bean
+	public FilterRegistrationBean corsFilterRegistration() {
+		FilterRegistrationBean registrationBean =
+				new FilterRegistrationBean(new CORSFilter());
+		registrationBean.setName("CORS Filter");
+		registrationBean.addUrlPatterns("/*");
+		registrationBean.setOrder(1);
+		return registrationBean;
 	}
 
 }
